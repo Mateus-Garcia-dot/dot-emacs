bcc8639759e9c70f4e992fd007302a11ae2f980d
Mateus Medeiros Garcia <69606533+Mateus-Garcia-dot@users.noreply.github.com>
2026-05-01 12:08:59 -0300

    Refactor a bit the treesitter
    Move the treesit language source list inside use-package

--- core/core-treesitter.el
+++ core/core-treesitter.el
@@ -4,24 +4,14 @@
   :ensure t
   :custom (treesit-auto-install 'prompt)
   :config
+  (setq treesit-language-source-alist
+        '((php        "https://github.com/tree-sitter/tree-sitter-php"        "v0.22.8" "php/src")
+          (phpdoc     "https://github.com/claytonrcarter/tree-sitter-phpdoc")
+          (html       "https://github.com/tree-sitter/tree-sitter-html"       "v0.20.3")
+          (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2")
+          (jsdoc      "https://github.com/tree-sitter/tree-sitter-jsdoc"      "v0.21.0")
+          (css        "https://github.com/tree-sitter/tree-sitter-css"        "v0.21.0")))
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
-
-(setq treesit-language-source-alist
-      '((php "https://github.com/tree-sitter/tree-sitter-php" "v0.22.8" "php/src")
-        (phpdoc "https://github.com/claytonrcarter/tree-sitter-phpdoc")
-        (html "https://github.com/tree-sitter/tree-sitter-html" "v0.20.3")
-        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2")
-        (jsdoc "https://github.com/tree-sitter/tree-sitter-jsdoc" "v0.21.0")
-        (css "https://github.com/tree-sitter/tree-sitter-css" "v0.21.0")))
-
-(treesit-install-language-grammar 'php)
-(treesit-install-language-grammar 'phpdoc)
-(treesit-install-language-grammar 'html)
-(treesit-install-language-grammar 'javascript)
-(treesit-install-language-grammar 'jsdoc)
-(treesit-install-language-grammar 'css)
-
-(add-to-list 'auto-mode-alist '("\\.php\\'" . php-ts-mode))
 
 (provide 'core-treesitter)
